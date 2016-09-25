d5bcd1a94c0b3b4a4b7018bd00c25268f7802d93
Timothy Baney <timbaney1989@gmail.com>
2016-09-24 21:29:38 -0500

    Make fixes to build process when running gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,16 +10,42 @@ var tsSrc = "process/typescript/"
 gulp.task('copylibs', function(){
 	return gulp
 		src([
-			'node_modules/es6-shim/es6-sham.min.js'
-			'node_modules/systemjs/dist/system-polyfills.js'
-			'node_modules/angular2/bundles/angular2-polyfills.js'
-			'node_modules/systemjs/dist/system.src.js'
-			'node_modules/rxjs/bundles/Rx.js'
-			'node_modules/angular2/bundles/angular2.dev.js'
+			'node_modules/es6-shim/es6-sham.min.js',
+			'node_modules/systemjs/dist/system-polyfills.js',
+			'node_modules/angular2/bundles/angular2-polyfills.js',
+			'node_modules/systemjs/dist/system.src.js',
+			'node_modules/rxjs/bundles/Rx.js',
+			'node_modules/angular2/bundles/angular2.dev.js',
 		])
-		.pipe(gulp.dest(appSrc + 'js/lib/angular2'))
-})
+		.pipe(gulp.dest(appSrc + 'js/lib/angular2'));
+});
 
 // When using Angular2, we don't use the entire framework. We only use what we need.
 // For example if we wanted http our router libraries, we would want to add them to 
 // the above task.
+
+gulp.task('typescript', function(){
+	return gulp
+		src(tsSrc + '**/*.ts')
+		.pipe(sourcemaps.init())
+		.pipe(typescript(tscConfig.compilerOptions))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(appSrc + 'js/'))
+});
+
+gulp.task('watch', function(){
+	gulp.watch(tsSrc + '**/*.ts', ['typescript']);
+	gulp.watch(appSrc + 'css/*.css', ['css']);
+	gulp.watch(appSrc + 'html/*.html', ['html']);
+});
+
+gulp.task('webserver', function(){
+	gulp.src(appSrc)
+		.pipe(webserver({
+			livereload: true,
+			open: true,
+			port: 9000
+		}));
+});
+
+gulp.task('default', ['copylibs', 'typescript', 'watch', 'webserver'])
